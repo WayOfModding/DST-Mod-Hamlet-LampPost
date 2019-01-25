ddffaa865276977654527e45ca8f096f1289aab7
KaiserKatze <donizyo@gmail.com>
2019-01-25 13:56:39 +0800

    Copy from Hamlet

---
+++ scripts/recipes.lua
@@ -0,0 +1,4 @@
+require "recipe"
+require "tuning"
+
+Recipe("city_lamp", {Ingredient("alloy", 1), Ingredient("transistor", 1),Ingredient("lantern",1)}, RECIPETABS.CITY,  TECH.CITY, RECIPE_GAME_TYPE.PORKLAND, "city_lamp_placer")
