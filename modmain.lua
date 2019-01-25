16920e0145849fa8889cdbe633614cec684503d4
KaiserKatze <donizyo@gmail.com>
2019-01-25 14:21:44 +0800

    Update

--- modmain.lua
+++ modmain.lua
@@ -1,1 +1,20 @@
 local require = GLOBAL.require
+
+Assets = {
+  Asset("ATLAS", "images/images.xml"),
+  Asset("IMAGE", "images/images.tex"),
+}
+
+local RECIPETABS = GLOBAL.RECIPETABS
+local TECH = GLOBAL.TECH
+
+local recipe_city_lamp = Recipe("city_lamp", {
+    Ingredient("gold", 1),
+    Ingredient("transistor", 1),
+    Ingredient("lantern",1)
+  },
+  RECIPETABS.LIGHT,
+  TECH.SCIENCE_TWO,
+  "city_lamp_placer")
+
+recipe_city_lamp.atlas = "images/images.xml"
