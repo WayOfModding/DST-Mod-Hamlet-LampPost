7704bdad6bfd2df77364b292d56ff256d38a5497
KaiserKatze <donizyo@gmail.com>
2020-03-20 20:54:35 +0800

    Update

--- modinfo.lua
+++ modinfo.lua
@@ -1,14 +1,15 @@
 --The name of the mod displayed in the 'mods' screen.
 name = "[Hamlet] Lamp Post"
 
+--A version number so you can ask people if they are running an old version of your mod.
+version = "1.1.4"
+
 --A description of the mod.
 description = "Lamp Post from Hamlet DLC of Don't Starve."
+description = "version "..version.."\n"..description
 
 --Who wrote this awesome mod?
 author = "KaiserKatze"
-
---A version number so you can ask people if they are running an old version of your mod.
-version = "1.1.3"
 
 --This lets other players know if your mod is out of date. This typically needs to be updated every time there's a new game update.
 api_version = 10
